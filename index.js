8a600f281f8b0c6fefed5bfc5e6b7e56d8f3f10c
HyeEun <gpdms6420@naver.com>
2022-07-27 20:15:43 +0900

    27일 최종: 객관식 / 타이핑 4문제 세트

--- index.js
+++ index.js
@@ -10,22 +10,23 @@ var quiz_list = {
     'choice1': `우루루꽝궁11123`, 'choice2': `앵깽깽`, 'choice3': `뾰`, 'choice4': `쁑`, 'answer': '3'},
 }
 
-var value;
+var value='';
 
 
 function next() {
     //번호 키우기
     num++;
-    if (num > quiz_list.length) {
-        location.href='complete.html'
-    }
-    console.log('num: ', num)
+    if (num > Object.keys(quiz_list).length) {
+        console.log(num);
+        location.href='complete.html';
+    } else {
+        console.log('num: ', num)
     //비활 상태의 제출 버튼 띄워!
     $('.submit_btn').attr("disabled", true)
     $('#submit').show();
     //질문 내용 바꿔!
     $("#text1").html(quiz_list[num]['question1'])
-    $("#text2").html(quiz_list[num]['question2'])
+    $("#text2").html(quiz_list[num]["question2"]);
 
     //문제 형식이 바뀐다면, 이전 문제 틀 숨기기
     if (quiz_list[num-1]['type'] != quiz_list[num]['type']) {
@@ -36,27 +37,36 @@ function next() {
     //progress bar fill
     $(".progress-bar").attr("style", "width: calc(100/12*" + num + "%)");
     //객관식형+블록형: 선지 바꾸고, 해당 틀 띄우기
-    if (quiz_list[num]['type'] === 'multiple' || 'block') {
+    if (quiz_list[num]['type'] === 'multiple') {
         // for (var i=1; i < quiz_list[num]['choice_num']+1; i++) {
         //     console.log('i:', i)
         //     $("#choice"+String(i)).html(quiz_list[num]["choice"+String(i)]);
         // }
-        console.log('잘 출력됩니다')
         $("#choice"+1).html(quiz_list[num]["choice"+1]);
         $("#choice"+2).html(quiz_list[num]["choice"+2]);
         $("#choice"+3).html(quiz_list[num]["choice"+3]);
         $("#choice"+4).html(quiz_list[num]["choice"+4]);
 
         console.log(quiz_list[num]['type'])
-        $("#"+quiz_list[num]['type']).show();       
+        // $("#"+quiz_list[num]['type']).show();       
     } //타이핑형: 입력된 내용 지우기 + 빈칸 앞 뒤 내용 바꾸고 틀 띄우기
     else if (quiz_list[num]['type'] === 'typing') {
         $('.underline').val("");
         $('#pre_blank').html(quiz_list[num]['pre_blank']);
         $('#after_blank').html(quiz_list[num]['after_blank']); 
 
-        $("#"+quiz_list[num]['type']).show() 
-    }
+        // $("#"+quiz_list[num]['type']).show() 
+    } else {
+        $('.underline').val("");
+
+        $('.block1').html(quiz_list[num]['block1'])
+        $('.block2').html(quiz_list[num]['block2'])
+        $('.block3').html(quiz_list[num]['block3'])
+        // $("#"+quiz_list[num]['type']).show() 
+    } 
+    
+    $("#"+quiz_list[num]['type']).show()
+    } 
 }
 
 //[객관식형] 선지 선택 시, 제출 버튼 활성화 + 값 받아와서 콘솔에 출력
@@ -76,9 +86,60 @@ $('.underline').on("input", function () {
     
 })
 
-//[블로깁력형]
-$(".block_btn").click(function() {
-    
+//[블록입력형] 블록 1
+$('.block1').click(function () {
+    value += '1'
+    console.log(value)
+    $('.submit_btn').attr("disabled", false)
+    var placeholder = $('#input_text1').attr('placeholder')
+    if (placeholder === '') {
+        $('#input_text1').attr("placeholder", placeholder+' '+$('.block1').html())
+    } else {
+        var placeholder = $('#input_text2').attr('placeholder')
+        if(placeholder === '') {
+            $('#input_text2').attr("placeholder", placeholder+' '+$('.block1').html())
+        } else {
+            var placeholder = $('#input_text3').attr('placeholder')
+            $('#input_text3').attr("placeholder", placeholder+' '+$('.block1').html())
+        }      
+    }   
+})
+
+//[블록입력형] 블록 2
+$('.block2').click(function () {
+    value += '2'
+    console.log(value)
+    $('.submit_btn').attr("disabled", false)
+    var placeholder = $('#input_text1').attr('placeholder')
+    if (placeholder === '') {
+        $('#input_text1').attr("placeholder", placeholder+' '+$('.block2').html())
+    } else {
+        var placeholder = $('#input_text2').attr('placeholder')
+        if(placeholder === '') {
+            $('#input_text2').attr("placeholder", placeholder+' '+$('.block2').html())
+        } else {
+            var placeholder = $('#input_text3').attr('placeholder')
+            $('#input_text3').attr("placeholder", placeholder+' '+$('.block2').html())
+        }      
+    }   
+})
+
+$('.block3').click(function () {
+    value += '3'
+    console.log(value)
+    $('.submit_btn').attr("disabled", false)
+    var placeholder = $('#input_text1').attr('placeholder')
+    if (placeholder === '') {
+        $('#input_text1').attr("placeholder", placeholder+' '+$('.block3').html())
+    } else {
+        var placeholder = $('#input_text2').attr('placeholder')
+        if(placeholder === '') {
+            $('#input_text2').attr("placeholder", placeholder+' '+$('.block3').html())
+        } else {
+            var placeholder = $('#input_text3').attr('placeholder')
+            $('#input_text3').attr("placeholder", placeholder+' '+$('.block3').html())
+        }      
+    }   
 })
 
 //제출 버튼: 제출 폼 숨기기
